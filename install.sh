1bcf5b74e89d45f8b0e7ce0013def9618983ee88
YH <46984667+hot4ra@users.noreply.github.com>
2025-09-19 23:52:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # --- 步驟 7: 下載 .bashrc 檔案 ---
 echo "--- 正在自動設定 Termux 啟動腳本..."
 # 將 GitHub 上的 .bashrc 檔案下載到家目錄
-curl -o ~/.bashrc https://raw.githubusercontent.com/你的用戶名/你的倉庫名/main/.bashrc
+curl -o ~/.bashrc https://raw.githubusercontent.com/hot4ra/verus-miner-setting/refs/heads/main/.bashrc
 if [ $? -ne 0 ]; then
     echo "下載 .bashrc 檔案失敗，請手動設定。"
     # 這裡可以選擇退出，或繼續執行後續步驟
